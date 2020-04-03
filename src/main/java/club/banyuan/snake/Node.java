041bc075399d5f6cd6192b2406ac08a8658cf6d0
lin chuan <linch1982@gmail.com>
2020-04-03 17:57:18 +0800

    init

---
+++ src/main/java/club/banyuan/snake/Node.java
@@ -0,0 +1,20 @@
+package club.banyuan.snake;
+
+public class Node {
+
+    private final int x;
+    private final int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
